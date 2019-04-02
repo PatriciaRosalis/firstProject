677367581259438d877280d008bb9b93d7cf44fe
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-04-02 17:55:00 +0100

    Day2- new change

--- js/frog.js
+++ js/frog.js
@@ -1,18 +1,20 @@
-class Frog {
-  constructor() {
-  }
-  randomImage() {
-    var $theImage = document.getElementsByClassName('divID');
-    var $placeIndex = Math.floor(Math.random() * $theImage.length);
-    var $frogArray = ['./Images/bomb.png', './Images/little-frog-1.png', './Images/animal-frog-001-s.png']
-    var $frogIndex = Math.floor(Math.random() * $frogArray.length);
+// class Frog {
+//   constructor() {
+//   }
+//   randomImage() {
+//     var $theImage = document.getElementsByClassName('pond');
+//     var $placeIndex = Math.floor(Math.random() * $theImage.length);
+//     var $frogArray = ['./Images/bomb.png', './Images/little-frog-1.png', './Images/animal-frog-001-s.png']
+//     var $frogIndex = Math.floor(Math.random() * $frogArray.length);
 
-    $theImage[$placeIndex].style.backgroundImage = "url(" + $frogArray[$frogIndex] + ")"
-    setTimeout(() => {
-      $theImage[$placeIndex].style.backgroundImage = "";
-    }, 1000);
-  }
+//     $theImage[$placeIndex].setAttribute("data-frog", Math.floor(Math.random()*2))
+//     // $theImage[$placeIndex].style.backgroundImage = "url(" + $frogArray[$frogIndex] + ")"
+//     setTimeout(() => {
+//       // $theImage[$placeIndex].style.backgroundImage = "";
+//       $theImage[$placeIndex].removeAttribute("data-frog")
+//   }, 1000);
+//   }
 
-}
+// }
 
 
--- js/main.js
+++ js/main.js
@@ -1,30 +1,63 @@
 var $instructions = document.getElementById('instructions');
 var $animal = document.getElementById('animal');
-var $littleFrog = new Frog();
-$rate = document.getElementById('rating')
+var $rate = document.querySelector('#rating p')
 
-var $rate;
 var frogNumber;
 var gameTimer;
 var output;
 var $numHits = 0;
 
 
-function onCloseIntrunctions() {
+function onCloseInstructions() {
   $instructions.style.display = "none";
 }
 
 setInterval(() => {
-  $littleFrog.randomImage()
+  randomImage()
 }, 2000);
 
 
-function hitFrog() {
-  $numHits += 1;
-  $rate = document.getElementById('rating').innerHTML = $numHits;
-  $littleFrog.randomImage();
+function hitFrog(pos) {
+  let dataFrog = document.querySelector("#pos-" + pos).getAttribute("data-frog")
+  console.log(dataFrog)
+
+  if (dataFrog >= 1) {
+    $numHits += 10;
+  }
+  else if (dataFrog >= 2) {
+    $numHits += 20;
+  }
+  else {
+    $numHits = 0;
+  }
+  $rate.innerHTML = $numHits;
+  randomImage();
 }
 
+function randomImage() {
+  var $theImage = document.getElementsByClassName('pond');
+  var $placeIndex = Math.floor(Math.random() * $theImage.length);
+  var $frogArray = ['./Images/bomb.png', './Images/little-frog-1.png', './Images/animal-frog-001-s.png']
+  var $frogIndex = Math.floor(Math.random() * $frogArray.length);
+
+  $theImage[$placeIndex].setAttribute("data-frog", Math.floor(Math.random() * 3) + 1)
+  // $theImage[$placeIndex].style.backgroundImage = "url(" + $frogArray[$frogIndex] + ")"
+  setTimeout(() => {
+    // $theImage[$placeIndex].style.backgroundImage = "";
+    $theImage[$placeIndex].removeAttribute("data-frog")
+  }, 1000);
+}
+
+
+// function gameOver() {
+
+//   var $over = document.getElementById("over");
+//   if ($over.style.display === "none") {
+//     $over.style.display = "block";
+//   } else {
+//     $over.style.display = "none";
+//   }
+// }
 // ----------------------------------------------------------------------------------------------------------------------------------
 // falta fazer o score + possivel high score + reset + play + click frogs + string no spongebob + game over
 // ----------------------------------------------------------------------------------------------------------------------------------
@@ -41,3 +74,12 @@ function hitFrog() {
 //   console.log("reset")
 // }
 
+
+
+// For the highscore
+// 1 high score
+// localStorage.getItem("highScore")
+// localStorage.setItem("highScore", 42)
+// multiple high scores
+// JSON.parse(localStorage.getItem("highScores"))
+// localStorage.setItem("highScores", JSON.stringify([42, 30, 10]))
